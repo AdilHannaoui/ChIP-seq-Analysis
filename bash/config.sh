536663a06e0d7a72b68ba9574e799c5035a142e3
Adil Hannaoui Anaaoui <154902011+AdilHannaoui@users.noreply.github.com>
2026-01-22 19:09:34 +0100

    Create config.sh

---
+++ bash/config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# ==================================================
+# Global configuration file for RNA-seq pipeline
+# ==================================================
+
+# ----------------------
+# Project directories
+# ----------------------
+WORKDIR="/mnt/c/Users/rna-seq"
+FASTQ_DIR="$WORKDIR/data"
+OUTPUT_DIR="$WORKDIR/output"
+
+# ----------------------
+# Computational resources
+# ----------------------
+THREADS=6
+
+# ----------------------
+# Tools and references
+# ----------------------
+
+# Trimmomatic
+TRIMMO_JAR="$WORKDIR/Trimmomatic-0.39/trimmomatic-0.39.jar"
+TRIMMOMATIC_ADAPTERS="$WORKDIR/Trimmomatic-0.39/adapters/TruSeq3-SE.fa"
+
+# BOWTIE2
+HISAT2_INDEX="$WORKDIR/Bowtie2/cerevisiae/index/genome"
+
+# ----------------------
+# Output subdirectories
+# ----------------------
+FASTQC_PRE_DIR="$OUTPUT_DIR/fastqc_pre"
+FASTQC_POST_DIR="$OUTPUT_DIR/fastqc_post"
+TRIMMED_FASTQ_DIR="$OUTPUT_DIR/fastq_trimmed"
+BOWTIE2_DIR="$OUTPUT_DIR/bowtie2"
+LOG_DIR="$OUTPUT_DIR/logs"
