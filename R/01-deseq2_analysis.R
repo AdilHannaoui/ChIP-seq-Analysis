0877729d1e6d4b46225c8e0d72c5d096486baf62
Adil Hannaoui Anaaoui <154902011+AdilHannaoui@users.noreply.github.com>
2026-01-31 19:29:04 +0100

    Update 01-deseq2_analysis.R

--- R/01-deseq2_analysis.R
+++ R/01-deseq2_analysis.R
@@ -6,41 +6,67 @@
 # --------------------------
 # Load configuration
 # --------------------------
-source("R/config.R") 
+source("R/config.R")
 
 library(DESeq2)
+library(dplyr)
 
 # --------------------------
 # Load data
 # --------------------------
 COUNTS_FILES <- list.files(
-    path = "output/macs2",
-    pattern = "_common_counts\\.txt$",
-    full.names = TRUE
+  path = "output/macs2",
+  pattern = "_common_counts\\.txt$",
+  full.names = TRUE
 )
 
 count_list <- lapply(COUNTS_FILES, function(f) {
-    read.table(f, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  read.table(f, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 })
 
+# Merge all count tables by genomic annotation columns
 counts_merged <- Reduce(function(x, y) merge(
-    x, y,
-    by = c("chrom","start","end","name","score","strand","Gene_id","Biotype"),
-    all = TRUE
+  x, y,
+  by = c("chrom","start","end","name","score","strand","Gene_id","Biotype"),
+  all = TRUE
 ), count_list)
 
-counts_matrix <- counts_merged[ , -(1:8)]
-rownames(counts_matrix) <- counts_merged$Gene_id
+# --------------------------
+# Collapse peaks by gene (SUM)
+# --------------------------
+count_data <- counts_merged %>%
+  select(-(chrom:Biotype)) %>%
+  mutate(Gene_id = counts_merged$Gene_id) %>%
+  group_by(Gene_id) %>%
+  summarise(across(everything(), sum, na.rm = TRUE))
 
-colData <- readRDS(SAMPLE_METADATA_PATH)       # metadata: samples x conditions
+counts_matrix <- as.data.frame(count_data)
+rownames(counts_matrix) <- counts_matrix$Gene_id
+counts_matrix$Gene_id <- NULL
 
-sample_group <- sub("_.*", "", CONDITIONS)
-condition <- sub(".*_", "", CONDITIONS)
+# --------------------------
+# Build colData automatically
+# --------------------------
+sample_names <- colnames(counts_matrix)
 
-colData$sample_group <- factor(sample_group)
-colData$condition <- factor(condition, levels = c("IN", "IP"))
+# Extract sample group (M1, M2, M3...) from filenames
+sample_groups <- sub("_common_counts\\.txt$", "", basename(COUNTS_FILES))
 
-colData$group_condition <- factor(paste0(colData$sample_group, "_", colData$condition))
+# Expand sample_groups to match IP/IN columns
+expanded_groups <- rep(sample_groups, each = 2)
+
+# Extract condition from column names
+conditions <- ifelse(grepl("^IP", sample_names), "IP", "IN")
+
+colData <- data.frame(
+  row.names = sample_names,
+  sample_group = factor(expanded_groups),
+  condition = factor(conditions, levels = c("IN", "IP"))
+)
+
+colData$group_condition <- factor(
+  paste0(colData$sample_group, "_", colData$condition)
+)
 
 # --------------------------
 # Create DESeqDataSet
@@ -60,6 +86,7 @@ dds <- dds[rowSums(counts(dds)) > MIN_COUNTS_FILTER, ]
 # Run DESeq2
 # --------------------------
 dds <- DESeq(dds)
+
 
 # --------------------------
 # Extract results for contrasts
