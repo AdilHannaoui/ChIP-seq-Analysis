51e8def812eb12da75dc21515bc905374199be15
Adil Hannaoui Anaaoui <154902011+AdilHannaoui@users.noreply.github.com>
2026-01-31 22:11:31 +0100

    Update 02-coordinates_extraction.R

--- R/02-coordinates_extraction.R
+++ R/02-coordinates_extraction.R
@@ -14,16 +14,22 @@ library(dplyr)
 # Load DESeq2 significant results
 # --------------------------
 res_WT_sig <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_WT_sig.rds"))
-res_A_sig  <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_A_sig.rds"))
-res_D_sig  <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_D_sig.rds"))
+res_M1_sig  <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_M1_sig.rds"))
+res_M12_sig  <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_M12_sig.rds"))
+
+res_WT_vs_M1_sig <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_WT_vs_M1_sig.rds"))
+res_M12_vs_M1_sig  <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_M12_vs_M1_sig.rds"))
+res_WT_vs_M12_sig  <- readRDS(file.path(OUTPUT_DIR, "DESeq2_res_WT_vs_M12_sig.rds"))
 
 # --------------------------
 # Load counts with coordinates
 # --------------------------
 counts_df <- readRDS(file.path(OUTPUT_DIR, "counts_merged.rds"))
+colnames(counts_df)
+
 
 # --------------------------
-# Merge coordinates with DESeq2 results
+# Merge individuals coordinates with DESeq2 results
 # --------------------------
 WT_sig_annot <- counts_df %>%
   filter(Gene_id %in% rownames(res_WT_sig)) %>%
@@ -32,17 +38,41 @@ WT_sig_annot <- counts_df %>%
     by = "Gene_id"
   )
 
-A_sig_annot <- counts_df %>%
-  filter(Gene_id %in% rownames(res_A_sig)) %>%
+M1_sig_annot <- counts_df %>%
+  filter(Gene_id %in% rownames(res_M1_sig)) %>%
   left_join(
-    as.data.frame(res_A_sig) %>% mutate(Gene_id = rownames(res_A_sig)),
+    as.data.frame(res_M1_sig) %>% mutate(Gene_id = rownames(res_M1_sig)),
     by = "Gene_id"
   )
 
-D_sig_annot <- counts_df %>%
-  filter(Gene_id %in% rownames(res_D_sig)) %>%
+M12_sig_annot <- counts_df %>%
+  filter(Gene_id %in% rownames(res_M12_sig)) %>%
   left_join(
-    as.data.frame(res_D_sig) %>% mutate(Gene_id = rownames(res_D_sig)),
+    as.data.frame(res_M12_sig) %>% mutate(Gene_id = rownames(res_M12_sig)),
+    by = "Gene_id"
+  )
+
+# --------------------------
+# Merge paired coordinates with DESeq2 results
+# --------------------------
+WT_M1_sig_annot <- counts_df %>%
+  filter(Gene_id %in% rownames(res_WT_vs_M1_sig)) %>%
+  left_join(
+    as.data.frame(res_WT_vs_M1_sig) %>% mutate(Gene_id = rownames(res_WT_vs_M1_sig)),
+    by = "Gene_id"
+  )
+
+M1_M12_sig_annot <- counts_df %>%
+  filter(Gene_id %in% rownames(res_M12_vs_M1_sig)) %>%
+  left_join(
+    as.data.frame(res_M12_vs_M1_sig) %>% mutate(Gene_id = rownames(res_M12_vs_M1_sig)),
+    by = "Gene_id"
+  )
+
+M12_WT_sig_annot <- counts_df %>%
+  filter(Gene_id %in% rownames(res_WT_vs_M12_sig)) %>%
+  left_join(
+    as.data.frame(res_WT_vs_M12_sig) %>% mutate(Gene_id = rownames(res_WT_vs_M12_sig)),
     by = "Gene_id"
   )
 
@@ -50,24 +80,42 @@ D_sig_annot <- counts_df %>%
 # Save annotated CSV tables
 # --------------------------
 write.csv(WT_sig_annot, file.path(OUTPUT_DIR, "WT_significant_peaks_with_coords.csv"), row.names = TRUE)
-write.csv(A_sig_annot,  file.path(OUTPUT_DIR, "A_significant_peaks_with_coords.csv"),  row.names = TRUE)
-write.csv(D_sig_annot,  file.path(OUTPUT_DIR, "D_significant_peaks_with_coords.csv"),  row.names = TRUE)
+write.csv(M1_sig_annot,  file.path(OUTPUT_DIR, "M1_significant_peaks_with_coords.csv"),  row.names = TRUE)
+write.csv(M12_sig_annot,  file.path(OUTPUT_DIR, "M12_significant_peaks_with_coords.csv"),  row.names = TRUE)
 
+write.csv(WT_M1_sig_annot, file.path(OUTPUT_DIR, "WT_M1_significant_peaks_with_coords.csv"), row.names = TRUE)
+write.csv(M1_M12_sig_annot,  file.path(OUTPUT_DIR, "M1_M12_significant_peaks_with_coords.csv"),  row.names = TRUE)
+write.csv(M12_WT_sig_annot,  file.path(OUTPUT_DIR, "M12_WT_significant_peaks_with_coords.csv"),  row.names = TRUE)
 # --------------------------
 # Export BED files for IGV
 # --------------------------
 WT_bed <- WT_sig_annot %>% select(chrom, start, end)
-A_bed  <- A_sig_annot  %>% select(chrom, start, end)
-D_bed  <- D_sig_annot  %>% select(chrom, start, end)
+M1_bed  <- M1_sig_annot  %>% select(chrom, start, end)
+M12_bed  <- M12_sig_annot  %>% select(chrom, start, end)
+
+WT_M1_bed <- WT_M1_sig_annot %>% select(chrom, start, end)
+M1_M12_bed  <- M1_M12_sig_annot  %>% select(chrom, start, end)
+M12_WT_bed  <- M12_WT_sig_annot  %>% select(chrom, start, end)
+
 
 write.table(WT_bed, file.path(OUTPUT_DIR, "WT_significant_peaks.bed"),
             sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
-write.table(A_bed,  file.path(OUTPUT_DIR, "A_significant_peaks.bed"),
+write.table(M1_bed,  file.path(OUTPUT_DIR, "M1_significant_peaks.bed"),
             sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
-write.table(D_bed,  file.path(OUTPUT_DIR, "D_significant_peaks.bed"),
+write.table(M12_bed,  file.path(OUTPUT_DIR, "M12_significant_peaks.bed"),
+            sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+
+
+write.table(WT_M1_bed, file.path(OUTPUT_DIR, "WT_M1_significant_peaks.bed"),
+            sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+write.table(M1_M12_bed,  file.path(OUTPUT_DIR, "M1_M12_significant_peaks.bed"),
+            sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+write.table(M12_WT_bed,  file.path(OUTPUT_DIR, "M12_WT_significant_peaks.bed"),
             sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
 cat("Peak coordinate extraction completed. CSV and BED files saved in:", OUTPUT_DIR, "\n")
-
